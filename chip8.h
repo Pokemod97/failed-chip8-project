91644bf80cc5974c3532cdf6674b1892fdce0d3a
caden <cadenkline9@gmail.com>
2017-07-27 21:53:04 -0400

    instruction set

---
+++ chip8.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "main.h" 
+
+void initChip(uint8_t *File);
---
+++ main.h
@@ -0,0 +1,4 @@
+#include <SDL2/SDL.h>
+#include <stdint.h>
+void Run();
+uint8_t * getFile();
